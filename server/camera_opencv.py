b22d6873278451f205c4db2fd58a5786c522fd99
yto <yto@ytodeiMac-2.local>
2020-02-29 20:26:54 +0800

    f

--- server/camera_opencv.py
+++ server/camera_opencv.py
@@ -49,5 +49,5 @@ class Camera(BaseCamera):
                 #if cv2.contourArea(c) > 1000:
                     #(x, y, w, h) = cv2.boundingRect(c)#计算轮廓线的外框，为当前帧画上外框
                     #cv2.rectangle(img,(x,y),(x+w,y+h), (0, 255, 0),1)# 更新文本
-            c#v2.putText(img, "action-detector", (10, 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            #cv2.putText(img, "action-detector", (10, 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
             yield cv2.imencode('.jpg', img)[1].tobytes()
